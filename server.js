c290db8468b8d59c101e6c4dc61107207b5e1b33
Bhotton25 <bhotton25@gmail.com>
2022-03-15 19:16:23 -0600

    More bugs

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(require('./routes'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Social-Profile-MongoDB', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-profile-mongodb', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
